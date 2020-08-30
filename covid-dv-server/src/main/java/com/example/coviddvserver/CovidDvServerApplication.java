249863b67072fc2d2bfabc741da1aabe2de0e64e
Pitney A. Jules <43119655+Mr-Pit@users.noreply.github.com>
2020-08-30 17:49:18 -0400

    Initialize project using Create React App

---
+++ covid-dv-server/src/main/java/com/example/coviddvserver/CovidDvServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.coviddvserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidDvServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidDvServerApplication.class, args);
+	}
+
+}
---
+++ covid-dv-server/src/test/java/com/example/coviddvserver/CovidDvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coviddvserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidDvServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
